6f35f7d52ca0098dd1209c10cf332987d43051c8
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2024-01-14 21:13:03 +0530

    Add routes for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ebook_app/home_page.dart';
+import 'package:flutter_ebook_app/navigation/routes.dart';
+import 'package:flutter_ebook_app/player_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +19,11 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomePage(),
+      routes: {
+        Routes.defaultRoute: (context) => const HomePage(),
+        Routes.home: (context) => const HomePage(),
+        Routes.playerScreen: (context) => const PlayerScreen(),
+      },
     );
   }
 }
-
---
+++ lib/navigation/routes.dart
@@ -0,0 +1,5 @@
+class Routes {
+  static const String home = "/home";
+  static const String defaultRoute = "/";
+  static const String playerScreen = "/playerScreen";
+}
